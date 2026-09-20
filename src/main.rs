4404f5c05dea296abd5283a52b848ef4cedbccba
twothirdshuman <axelwas07@gmail.com>
2026-09-20 20:27:11 +0200

    mouse clicks and stateful model

--- src/main.rs
+++ src/main.rs
@@ -54,20 +54,21 @@ impl Textures {
 
 struct Model {
     textures: Textures,
-    game: chess_box::game::ChessGame
+    game: chess_box::game::ChessGame,
+    highlighted: Option<(usize, usize)>
 }
 
 fn main() {
-    nannou::app(model).run();
+    nannou::app(model).update(update).run();
 }
 
 fn model(app: &App) -> Model {
     app.new_window().size(720, 720).view(view).build();
 
-    Model { textures: Textures::load(app), game: chess_box::game::ChessGame::new_standard_game() }
+    Model { textures: Textures::load(app), game: chess_box::game::ChessGame::new_standard_game(), highlighted: None }
 }
 
-fn draw_background(app: &App) {
+fn draw_background(app: &App, model: &Model) {
     let draw = app.draw();
 
     let win = app.window_rect();
@@ -78,11 +79,17 @@ fn draw_background(app: &App) {
         for r in 0..8 {
             let light = Color::srgb(240.0 / 256.0, 217.0 / 256.0, 181.0 / 256.0);
             let dark = Color::srgb(181.0 / 256.0, 136.0 / 256.0, 99.0 / 256.0);
-            let to_use = if (c + r) % 2 == 1 {
+            let highlighted_color = Color::srgb(1.0, 0.0, 0.0);
+            
+            let mut to_use = if (c + r) % 2 == 1 {
                 light
             } else {
                 dark
             };
+
+            if Some((c, r)) == model.highlighted {
+                to_use = highlighted_color;
+            }
 
             draw.rect().color(to_use)
                 .x(win.left() + c as f32 * (width / 8.0) + width / 16.0)
@@ -118,26 +125,33 @@ fn draw_pieces(app: &App, model: &Model) {
     }
 }
 
-fn mouse_logic(app: &App, model: &Model) {
+fn mouse_logic(app: &App, model: &mut Model) {
     if !app.mouse_buttons().any_just_pressed([MouseButton::Left]) {
         return;
     }
     let pos = app.mouse();
 
     let diff_x = (app.window_rect().left() - pos.x).abs();
-    let diff_y = (app.window_rect().top() - pos.y).abs();
+    let diff_y = (app.window_rect().bottom() - pos.y).abs();
 
     let column = (diff_x / (app.window_rect().w() / 8.0)).floor() as usize;
     let row = (diff_y / (app.window_rect().h() / 8.0)).floor() as usize;
 
-    println!("{:?}", (column, row));
+    if model.highlighted == Some((column, row)) {
+        model.highlighted = None;
+    } else {
+        model.highlighted = Some((column, row));
+    }
 }
 
 fn view(app: &App, model: &Model) {
 
-    draw_background(app);
+    draw_background(app, model);
 
     draw_pieces(app, model);
 
+}
+
+fn update(app: &App, model: &mut Model) {
     mouse_logic(app, model);
 }
