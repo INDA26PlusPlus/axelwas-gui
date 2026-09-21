57bf336d23072c319c3c66f406121f239fc06d29
TwoThirdsHuman <twothirdshuman@users.noreply.github.com>
2026-09-21 16:05:13 +0200

    done done done

--- src/main.rs
+++ src/main.rs
@@ -137,6 +137,8 @@ fn draw_pieces(app: &App, model: &Model) {
 }
 
 fn draw_dialog(app: &App, model: &Model) {
+    match model.dialog_box { None => return, Some(_) => () };
+
     let draw = app.draw();
     draw.rect().color(Color::srgb(0.0, 0.0, 0.0)).x(0.0).y(0.0).w(300.0).h(300.0);
     draw.text("♕ Queen\n♖ Rook\n♘ Knight\n♗ Bishop").font_size(40).x(0.0).y(0.0); 
